b489701fad48af6ab1f337874e949f4ba36dd2c6
Ilya Bogatov <wolfofdeath@yandex.ru>
2022-08-04 02:01:02 +0300

    Add format and files to docopt arguments

--- src/Cli.php
+++ src/Cli.php
@@ -12,10 +12,12 @@ function getAppArguments(): Response
     Usage:
       gendiff (-h|--help)
       gendiff (-v|--version)
+      gendiff [--format <fmt>] <firstFile> <secondFile>
     
     Options:
       -h --help                     Show this screen
       -v --version                  Show version
+      --format <fmt>                Report format [default: stylish]
     DOCOPT;
 
     return Docopt::handle($doc);
